132675528116cdc6c9836782bb498522b1b6217a
Eoin Crosbie <eoin.crosbie@feedhenry.com>
2013-06-10 15:23:28 +0100

    Add dummy login code cloud side

--- cloud/backend.js
+++ cloud/backend.js
@@ -5,16 +5,49 @@
 
 
 /*
- * Login Call Function
+ * Login Function
  *
- * @user String 		- username
- * @pass String 		- password
- * @cb function 		- callback function
+ * @param {String} 		- username
+ * @param {String} 		- password
+ * @param {function} 	- callback function
  */
-exports.login = function(user,pass,cb){
-	// TODO: make call to backend
-	return cb(true);
-};
+exports.login = function(params, cb){
+	var self = this;
+	var user = params.user;
+	var pass = params.pass;
+	var res  = {};
+	if(!user || !pass){
+		res.status = false;
+		res.error  = "Username or Password missing";
+		return cb(null, res); // FAIL
+	} else {
+		self.backendCall('login', params, function(success, data){
+			if(success){
+				res.status = true;
+				res.error  = null;
+				return cb(null, res); // SUCCESS
+			} else {
+				res.status = false;
+				res.error  = "Incorrect Username or Password";
+				return cb(null, res); // FAIL
+			}
+		});
+	}
+},
+
+
+/*
+ * Backend Call function
+ * All backend functions use this to access the external system
+ * @param {String} 		- function name on backend
+ * @param {object} 		- params object
+ * @param {function}	- callback
+ */
+exports.backendCall = function(fn, params, cb){
+	// TODO: make external web call
+	return cb(true, {test: 'test'});
+}
+
 
 
 
--- cloud/main.js
+++ cloud/main.js
@@ -18,24 +18,7 @@ exports.getConfig = function(params, cb){
  * Save error callback for network issues
  */
 exports.login = function(params, cb){
-	var user = params.user;
-	var pass = params.pass;
-	var res  = {};
-	if(!user || !pass){
-		res.status = false;
-		res.error  = "Username or Password missing";
-		return cb(null, res);
-	} else {
-		Backend.login(user,pass, function(success){
-			if(success){
-				res.status = true;
-				res.error  = null;
-				return cb(null, res);
-			} else {
-				res.status = false;
-				res.error  = "Incorrect Username or Password";
-				return cb(null, res);
-			}
-		});
-	}
+	Backend.login(params, function(err, data){
+		return cb(err, data);
+	})
 }
